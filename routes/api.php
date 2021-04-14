fe39e4dc7d66a128b4c30730a6a6d8df978f750a
Fajar AM <somabangsa@gmail.com>
2021-04-14 09:12:52 +0700

    update routing

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\Rest\RestAccountController;
+use App\Http\Controllers\Rest\RestMasterDataController;
+use App\Http\Controllers\Rest\RestMusyrifManagementController;
+use App\Http\Controllers\Rest\RestStudentActivityManagementController;
 use Illuminate\Http\Request;
 use App\Models\Siswa;
 use Carbon\Carbon;
@@ -21,40 +25,39 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
  
 //AUTO api prefixed
 Route::prefix('account')->group(function () {
-    Route::post('login', 'Rest\RestAccountController@login');
+    Route::post('login', [RestAccountController::class, 'login']);
     // Route::post('register', 'Rest\RestAccountController@register');
 
-    Route::post('requestid', 'Rest\RestAccountController@requestId')->name('requestid');
+    Route::post('requestid', [RestAccountController::class, 'requestId'])->name('requestid');
    
 });
 Route:: group(['prefix' => 'musyrifmanagement' , 'middleware'=>['auth:api', 'role:admin_asrama']  ], function () {
     
-    Route::post('activate', 'Rest\RestMusyrifManagementController@activate');
+    Route::post('activate', [RestMusyrifManagementController::class, 'activate']);
      
 });
 Route:: group(['prefix' => 'masterdata' , 'middleware'=>['auth:api', 'role:admin_asrama,musyrif_asrama']  ], function () {
     //RestMusyrifManagementController
-    Route::post('list', 'Rest\RestMasterDataController@list');
-    Route::post('update', 'Rest\RestMasterDataController@update');
-    Route::post('delete', 'Rest\RestMasterDataController@delete');
-    Route::post('getbyid', 'Rest\RestMasterDataController@getbyid');
+    Route::post('list', [RestMasterDataController::class, 'list']);
+    Route::post('update', [RestMasterDataController::class, 'update']);
+    Route::post('delete', [RestMasterDataController::class, 'delete']);
+    Route::post('getbyid', [RestMasterDataController::class, 'getbyid']);
     
      
 });
 Route:: group(['prefix' => 'accountdashboard' , 'middleware'=>'auth:api'  ], function () {
     
-    Route::post('logout', 'Rest\RestAccountController@logout');
-    Route::post('requestid', 'Rest\RestAccountController@requestId');
+    Route::post('logout', [RestAccountController::class, 'logout']);
+    Route::post('requestid', [RestAccountController::class, 'requestId']);
     
 });
 Route:: group(['prefix' => 'dormitorymanagement' , 'middleware'=>['auth:api', 'role:musyrif_asrama']  ], function () {
     //RestMusyrifManagementController
-    Route::post('classes', 'Rest\RestStudentActivityManagementController@classes');
-    Route::post('submitpointrecord', 'Rest\RestStudentActivityManagementController@submitpointrecord');
-    Route::post('submitmedicalrecord', 'Rest\RestStudentActivityManagementController@submitmedicalrecord');
-    Route::post('monthlymedicalrecord', 'Rest\RestStudentActivityManagementController@monthlymedicalrecord');
-    Route::post('droppoint', 'Rest\RestStudentActivityManagementController@droppoint');
-
-
-    // Route::post('activate', 'Rest\RestMusyrifManagementController@activate');
+    Route::post('classes', [RestStudentActivityManagementController::class, 'classes']);
+    Route::post('submitpointrecord', [RestStudentActivityManagementController::class, 'submitpointrecord']);
+    Route::post('submitmedicalrecord', [RestStudentActivityManagementController::class, 'submitmedicalrecord']);
+    Route::post('monthlymedicalrecord', [RestStudentActivityManagementController::class, 'monthlymedicalrecord']);
+    Route::post('droppoint', [RestStudentActivityManagementController::class, 'droppoint']);
+ 
 });
+//
